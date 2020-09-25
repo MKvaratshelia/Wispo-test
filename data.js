654d9323181c31475547536e57f2fa39aa70eef4
Kvaratshelia Mihail <kvaratsheliamihail@yandex.ru>
2020-09-25 22:13:14 +0300

    Загрузил все файлы

---
+++ data.js
@@ -0,0 +1,44 @@
+const DATA = [
+  {
+    title: "Яблоки",
+    image:
+      "https://cdn.segodnya.ua/i/image_650x434/media/image/5d6/f90/530/5d6f9053031b6.jpg",
+    description: "Великолепные яблоки",
+    price: 200,
+  },
+  {
+    title: "Яблоки",
+    image:
+      "https://cdn.segodnya.ua/i/image_650x434/media/image/5d6/f90/530/5d6f9053031b6.jpg",
+    description: "Великолепные яблоки",
+    price: 150,
+  },
+  {
+    title: "Яблоки",
+    image:
+      "https://cdn.segodnya.ua/i/image_650x434/media/image/5d6/f90/530/5d6f9053031b6.jpg",
+    description: "Великолепные яблоки",
+    price: 100,
+  },
+  {
+    title: "Яблоки",
+    image:
+      "https://cdn.segodnya.ua/i/image_650x434/media/image/5d6/f90/530/5d6f9053031b6.jpg",
+    description: "Великолепные яблоки",
+    price: 400,
+  },
+  {
+    title: "Яблоки",
+    image:
+      "https://cdn.segodnya.ua/i/image_650x434/media/image/5d6/f90/530/5d6f9053031b6.jpg",
+    description: "Великолепные яблоки",
+    price: 220,
+  },
+  {
+    title: "Яблоки",
+    image:
+      "https://cdn.segodnya.ua/i/image_650x434/media/image/5d6/f90/530/5d6f9053031b6.jpg",
+    description: "Великолепные яблоки",
+    price: 600,
+  },
+];
---
+++ index.js
@@ -0,0 +1,75 @@
+let is = true;
+const f = () => (!is ? (is = true) : (is = false));
+
+console.log(f());
+console.log(f());
+console.log(f());
+
+class Card {
+  createCard(data) {
+    const { title, description, image, price } = data;
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+    <h1 class="card__title">${title}</h1>
+    <img
+      src=${image}
+      alt=${title}
+      srcset=""
+      class="card__image"
+    />
+    <p class="card__description">${description}</p>
+    <span class="card__price">${price} ₽</span>
+    `;
+    return card;
+  }
+}
+
+class CardList {
+  constructor(card, container, select, data) {
+    this.select = select;
+    this.card = card;
+    this.container = container;
+    this.data = data;
+
+    this.select.addEventListener("change", this.sort.bind(this));
+  }
+
+  render() {
+    for (let item of this.data) {
+      container.insertAdjacentElement("beforeend", card.createCard(item));
+    }
+  }
+
+  clearCarrdList() {
+    while (this.container.firstChild) {
+      this.container.removeChild(this.container.firstChild);
+    }
+  }
+
+  sort() {
+    let value = this.select.value;
+    if (value === "up") {
+      this.data.sort((a, b) => {
+        return a.price - b.price;
+      });
+      this.clearCarrdList();
+      this.render();
+    }
+
+    if (value === "down") {
+      this.data.sort((a, b) => {
+        return b.price - a.price;
+      });
+      this.clearCarrdList();
+      this.render();
+    }
+  }
+}
+
+const select = document.querySelector(".select");
+const container = document.querySelector(".card-list");
+const card = new Card();
+const cardList = new CardList(card, container, select, DATA);
+
+cardList.render();
